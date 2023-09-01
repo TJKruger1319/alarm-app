0dc6a13670b14045ebb35611b6dbeef86bb1c305
TJ <TJKruger1319@gmail.com>
2023-08-31 22:15:58 -0400

    Updates backend for Render

--- alarm_backend/app.py
+++ alarm_backend/app.py
@@ -2,14 +2,14 @@ from flask import Flask, jsonify, request, make_response
 from models import db, connect_db, Alarm
 from helper import makeDict, generate_math_prompt, serialize, generate_random_sentence
 from flask_cors import CORS
-
+import os
 
 app = Flask(__name__)
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///alarmdb'
+app.config['SQLALCHEMY_DATABASE_URI'] =  os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SECRET_KEY'] = "I'LL NEVER TELL!!"
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 app.app_context().push()
 connect_db(app)
